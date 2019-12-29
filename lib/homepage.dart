3f3ea2465bbbba4b7128354f63ba8bc453e70b4f
hayatullah farahi <hayatullah.farahi@gmail.com>
2019-12-29 18:10:14 +0530

    first commit

---
+++ lib/homepage.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class MyHome extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MyHomePage(title: 'Flutter Demo Home Page');
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  FirebaseAuth _auth = FirebaseAuth.instance;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You are logged in now!',
+            ),
+            RaisedButton(
+              child: Text('Logout'),
+              onPressed: () async {
+                await _auth.signOut();
+                Navigator.of(context).pushReplacementNamed('/loginpage');
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
